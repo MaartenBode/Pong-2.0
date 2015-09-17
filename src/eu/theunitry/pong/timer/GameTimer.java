0becbdb18b581b06c025d94b4b789df336f64fe7
Maarten Bode <maartenbodepc@gmail.com>
2015-09-17 12:02:47 +0200

    Made timer private

--- src/eu/theunitry/pong/timer/GameTimer.java
+++ src/eu/theunitry/pong/timer/GameTimer.java
@@ -6,16 +6,20 @@ import eu.theunitry.pong.gui.GraphicsPanel;
 
 public class GameTimer
 {
-	public Timer timer;
-	public GraphicsPanel graphicsPanel;
-	public static final int TICK = 5;
+	private Timer timer;
+	private GraphicsPanel graphicsPanel;
+	private static final int TICK = 5;
 	
 	public GameTimer(GraphicsPanel panel)
 	{
 		this.graphicsPanel = panel;
 		this.timer = new Timer(TICK, graphicsPanel);
-		this.timer.start();
+		this.start();
 	}
 	
+	public void start()
+	{
+		this.timer.start();
+	}
 	
 }
